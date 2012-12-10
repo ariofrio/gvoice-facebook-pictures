8038392f12e742ed94d8a30ad205971be04413d8
Andres Riofrio <riofrios@gmail.com>
2012-12-09 17:48:14 -0800

    Insert facebook pictures; it's alive.

--- injected.js
+++ injected.js
@@ -1,27 +1,38 @@
-function insertListener(event) {
-  if(event.animationName == "nodeInserted") {
-    replaceWithPicture(event.target);
+(function() {
+  var facebookFriends = {};
+
+  function insertListener(event) {
+    if(event.animationName == "nodeInserted") {
+      replaceWithPicture(event.target);
+    }
   }
-}
 
-function replaceWithPicture(img) {
-  try {
-    var metadata = img.parentNode.parentNode.nextElementSibling;
-    var name = metadata.querySelector(".gc-message-name-link").textContent;
-  } catch(e) {
-    console.warn(e);
+  function replaceWithPicture(img) {
+    try {
+      var metadata = img.parentNode.parentNode.nextElementSibling;
+      var name = metadata.querySelector(".gc-message-name-link").textContent;
+      if(name in facebookFriends) {
+        img.src = "https://graph.facebook.com/" + facebookFriends[name] + "/picture?return_ssl_resources=1&width=64&height=64"
+      }
+    } catch(e) {
+      console.warn(e);
+    }
   }
-  img.src = "http://www.demilked.com/magazine/wp-content/uploads/2012/05/funny-portraits-blow-job-tadas-cerniauskas-thumb45.jpg";
-}
 
-window.fbAsyncInit = function() {
-  setTimeout(function() {
-    FB.init({
-      appId: '120425281453003',
-      status: true,
-      cookie: true
-    });
+  function handlePageOfFriends(response) {
+    if(response.data) {
+      for(var i=0; i<response.data.length; i++) {
+        facebookFriends[response.data[i].name] = response.data[i].id;
+      }
+    }
+    if(response.paging && response.paging.next) {
+      FB.api(response.paging.next, handlePageOfFriends);
+    } else {
+      friendsLoaded();
+    }
+  }
 
+  function friendsLoaded() {
     document.addEventListener("animationstart", insertListener, false);
     document.addEventListener("MSAnimationStart", insertListener, false);
     document.addEventListener("webkitAnimationStart", insertListener, false);
@@ -31,8 +42,31 @@ window.fbAsyncInit = function() {
     for(var i=0; i<imgs.length; i++) {
       replaceWithPicture(imgs[i]);
     }
-  }, 1000);
-}
+  }
+
+  function loggedIn() {
+    FB.api('/me/friends', handlePageOfFriends);
+  }
+
+  window.fbAsyncInit = function() {
+      FB.init({
+        appId: '120425281453003',
+        status: true,
+        cookie: true
+      });
+      FB.getLoginStatus(function(response) {
+        if(response.status === 'connected') {
+          loggedIn();
+        } else {
+          FB.login(function(response) {
+            if(response.authResponse) {
+              loggedIn();
+            }
+          });
+        }
+      });
+  }
+})();
 
 // Load the Facebook SDK asynchronously
 (function(d, debug){
